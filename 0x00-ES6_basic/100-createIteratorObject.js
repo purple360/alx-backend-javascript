0f6df734b06ef3e511eaf68e7eb2bd17a828dd54
purple360 <64763854+purple360@users.noreply.github.com>
2023-09-26 02:22:06 +0000

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,51 +1,25 @@
-// The Breakdown 😏😉😉
-// I know it's long, I was just trying to understand how things work
-//
-// Here's the shorter code using generator function
-//
 export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
+  const departments = Object.values(report);
+
+  let currentDeptIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  return {
+    next() {
+      if (currentDeptIndex < departments.length) {
+        const currentDepartment = departments[currentDeptIndex];
+        if (currentEmployeeIndex < currentDepartment.employees.length) {
+          const employee = currentDepartment.employees[currentEmployeeIndex];
+          currentEmployeeIndex++;
+          return { value: employee, done: false };
+        } else {
+          // Move to the next department
+          currentDeptIndex++;
+          currentEmployeeIndex = 0;
+          return this.next(); // Recursively call next() for the next department
+        }
       }
-    }
-  }());
+      return { done: true };
+    },
+  };
 }
-
-//
-// Now, the longer code
-// export default function createIteratorObject(report) {
-//   const listObject = Object.keys(report.allEmployees);
-//   let currentKey = 0;
-//   let currentIndex = 0;
-//   let result = {};
-//   return {
-//     [Symbol.iterator]() {
-//       return {
-//         next() {
-//           if (currentKey === listObject.length) {
-//             return {
-//               value: undefined,
-//               done: true,
-//             };
-//           }
-
-//           const array = report[listObject[currentKey]];
-//           if (currentIndex < array.length) {
-//             result = {
-//               value: array[currentIndex],
-//               done: false,
-//             };
-//             currentIndex += 1;
-//             if (currentIndex === array.length) {
-//               currentKey += 1;
-//               currentIndex = 0;
-//             }
-//           }
-//           return result;
-//         },
-//       };
-//     },
-//   };
-// }
